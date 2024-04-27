35729e79f82976b6580999c252cac8f0189e20cb
Aleksandre <aleksa_dzneladze@yahoo.com>
2024-04-27 14:46:19 +0400

    Added "GetPersonLinks" method

--- Handbook.Repository/PersonLinkRepository.cs
+++ Handbook.Repository/PersonLinkRepository.cs
@@ -1,12 +1,27 @@
 ﻿using Handbook.DTO;
 using Handbook.Service.RepositoryInterfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace Handbook.Repository;
 
 internal class PersonLinkRepository : RepositoryBase<PersonLink>, IPersonLinkRepository
 {
+    protected HandbookDbContext _context;
+
     internal PersonLinkRepository(HandbookDbContext context) : base(context)
     {
+        _context = context;
+    }
 
+    internal IEnumerable<Person> GetPeople(int id)
+    {
+         return _context.PersonLinks.Where(x => x.PersonFrom.Id == id)
+            .Select(t => t.PersonTo)
+            .Concat(
+                _context.PersonLinks
+                .Where(x => x.PersonTo.Id == id)
+                .Select(f => f.PersonFrom))
+            .AsNoTracking()
+            .ToList();
     }
 }
